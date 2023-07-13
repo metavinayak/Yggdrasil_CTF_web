266fa13b87c8192b0aea9e1078f454e7089df984
Vinayak Sachan <vinayakasachan@gmail.com>
2023-07-13 14:49:21 +0530

    Docker stuff

--- app.py
+++ app.py
@@ -56,5 +56,5 @@ def displayhint():
     return response
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', port=3000, debug=True)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
+    # app.run(debug=True)
